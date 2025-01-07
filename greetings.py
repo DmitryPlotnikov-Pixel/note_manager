28f1f3a613613fe3ef9fa01afc8e0bc1233342bb
DmitryPlotnikov-Pixel <daplotnikov.rzd@gmail.com>
2025-01-07 22:01:02 +0500

    Grade 1, Этап 1, Задание 1

---
+++ greetings.py
@@ -0,0 +1,7 @@
+user_name, title, content, status, created_date, issue_date = "Дмитрий", "Задачи на январь", "Что необходимо сделать в январе 2025 года обязательно", "В процессе", "01.01.2025", "31.01.2025"
+print("Имя пользователя:", user_name)
+print("Заголовок:", title)
+print("Описание:", content)
+print("Статус:", status)
+print("Дата создания заметки:", created_date)
+print("Срок выполнения:", issue_date)
