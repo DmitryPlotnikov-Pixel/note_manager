cf29bac40c4175b9853acf077646bb5bc3f4aafa
DmitryPlotnikov-Pixel <daplotnikov.rzd@gmail.com>
2025-01-07 22:49:45 +0500

    Date_changer
    Введен показ более лаконичной даты

---
+++ date_changer.py
@@ -0,0 +1,7 @@
+user_name, title, content, status, created_date, issue_date = "Дмитрий", "Задачи на январь", "Что необходимо сделать в январе 2025 года обязательно", "В процессе", "01-Jan-2025", "31-Jan-2025"
+print("Имя пользователя:", user_name)
+print("Заголовок:", title)
+print("Описание:", content)
+print("Статус:", status)
+print("Дата создания заметки:", created_date[0:6])
+print("Срок выполнения:", issue_date[0:6])
