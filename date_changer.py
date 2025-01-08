4c71e1452954547db90ba2729421c816a96e9b78
DmitryPlotnikov-Pixel <daplotnikov.rzd@gmail.com>
2025-01-08 21:48:09 +0500

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,7 +1,2 @@
 user_name, title, content, status, created_date, issue_date = "Дмитрий", "Задачи на январь", "Что необходимо сделать в январе 2025 года обязательно", "В процессе", "01-Jan-2025", "31-Jan-2025"
-print("Имя пользователя:", user_name)
-print("Заголовок:", title)
-print("Описание:", content)
-print("Статус:", status)
-print("Дата создания заметки:", created_date[0:6])
-print("Срок выполнения:", issue_date[0:6])
+print("Имя пользователя:", user_name + "\n", "Заголовок:", title + "\n", "Описание:", content + "\n", "Статус:", status + "\n", "Дата создания заметки:", created_date[0:6] + "\n", "Срок выполнения:", issue_date[0:6])
