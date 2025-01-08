b3e1f9af0598a58f7877dc5ed38b342bc631a341
DmitryPlotnikov-Pixel <daplotnikov.rzd@gmail.com>
2025-01-08 21:47:06 +0500

    greetings.py

--- greetings.py
+++ greetings.py
@@ -1,7 +1,2 @@
 user_name, title, content, status, created_date, issue_date = "Дмитрий", "Задачи на январь", "Что необходимо сделать в январе 2025 года обязательно", "В процессе", "01.01.2025", "31.01.2025"
-print("Имя пользователя:", user_name)
-print("Заголовок:", title)
-print("Описание:", content)
-print("Статус:", status)
-print("Дата создания заметки:", created_date)
-print("Срок выполнения:", issue_date)
+print("Имя пользователя:", user_name + "\n", "Заголовок:", title + "\n", "Описание:", content + "\n", "Статус:", status + "\n", "Дата создания заметки:", created_date + "\n", "Срок выполнения:", issue_date)
