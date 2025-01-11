8779e652b888759f0ac324f5c208aeba5135a8bd
DmitryPlotnikov-Pixel <daplotnikov.rzd@gmail.com>
2025-01-11 18:15:30 +0500

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,4 +1,4 @@
 created_date = "01-Jan-2025"
 issue_date = "31-Jan-2025"
-print("Дата создания:", created_date[0:6])
-print("Дедлайн:", issue_date[0:6])
+print("Дата создания заметки: ", created_date[0:6])
+print("Срок выполнения: ", issue_date[0:6])
